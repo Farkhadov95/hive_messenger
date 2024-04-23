63b541219406845dc5ff91b648b76bd3fca23ef8
Farkhadov95 <farkhadov950405@gmail.com>
2024-04-23 11:50:28 +0500

    added title and menu button

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -1,13 +1,23 @@
-import { Avatar, HStack, Box, Text } from "@chakra-ui/react";
+import { Avatar, HStack, Box, Text, Button } from "@chakra-ui/react";
+import { HiDotsVertical } from "react-icons/hi";
 
 const ChatHeader = () => {
   return (
-    <HStack padding={2} bgColor={"white"} textColor={"black"} gap={3}>
-      <Avatar />
-      <Box lineHeight={"1.5em"}>
-        <Text fontWeight={"bold"}>Name</Text>
-        <Text textColor={"gray"}>last message</Text>
-      </Box>
+    <HStack padding={2} bgColor={"white"} justify={"space-between"}>
+      <HStack textColor={"black"} gap={3}>
+        <Avatar />
+        <Box lineHeight={"1.5em"}>
+          <Text fontWeight={"bold"}>Name</Text>
+          <Text textColor={"gray"}>last message</Text>
+        </Box>
+      </HStack>
+      <Button
+        as={HiDotsVertical}
+        color={"gray.400"}
+        width={"fit-content"}
+        height={"20px"}
+        padding={0}
+      />
     </HStack>
   );
 };
