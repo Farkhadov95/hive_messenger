b8e257c95d53dd74d53996c3bcf6976a11267531
Farkhadov95 <farkhadov950405@gmail.com>
2024-05-13 16:44:14 +0500

    Router. WIP

--- src/router/PublicRoutes.tsx
+++ src/router/PublicRoutes.tsx
@@ -4,15 +4,17 @@ import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
 import NotFound from "../pages/NotFound";
 import PrivateRoutes from "./PrivateRoutes";
+import Chat from "../pages/Chat";
+import Chats from "../pages/Chats";
 import Welcome from "../pages/Welcome";
 
 export const routes = {
   welcome: "/",
-  home: "/home",
-  login: "/login",
-  signup: "/signup",
-  chats: "/chats",
-  chat: "/chat/:id",
+  home: "home",
+  login: "login",
+  signup: "signup",
+  chats: "chats",
+  chat: "chat/:id",
   notFound: "*",
 };
 
@@ -20,21 +22,31 @@ const router = createBrowserRouter([
   {
     path: routes.welcome,
     element: <Welcome />,
-  },
-  {
-    path: routes.login,
-    element: <Login />,
-  },
-  {
-    path: routes.signup,
-    element: <SignUp />,
-  },
-  {
-    element: <PrivateRoutes />,
     children: [
       {
-        path: routes.home,
-        element: <Home />,
+        path: routes.login,
+        element: <Login />,
+      },
+      {
+        path: routes.signup,
+        element: <SignUp />,
+      },
+      {
+        element: <PrivateRoutes />,
+        children: [
+          {
+            path: routes.home,
+            element: <Home />,
+          },
+          {
+            path: routes.chats,
+            element: <Chats />,
+          },
+          {
+            path: routes.chat,
+            element: <Chat />,
+          },
+        ],
       },
     ],
   },
