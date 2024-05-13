286aed0fd67afe6d31be84e9c7d84f1292901f22
Farkhadov95 <farkhadov950405@gmail.com>
2024-05-13 18:27:23 +0500

    Router. WIP

--- src/router/PublicRoutes.tsx
+++ src/router/PublicRoutes.tsx
@@ -4,76 +4,42 @@ import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
 import NotFound from "../pages/NotFound";
 import PrivateRoutes from "./PrivateRoutes";
-import Welcome from "../pages/Welcome";
 
 export const routes = {
   welcome: "/",
-  home: "home",
-  login: "login",
-  signup: "signup",
-  chats: "chats",
-  chat: "chat/:id",
+  home: "/home",
+  login: "/login",
+  signup: "/signup",
+  chats: "/chats",
+  chat: "/chat/:id",
   notFound: "*",
 };
 
-// const router = createBrowserRouter([
-//   {
-//     path: routes.welcome,
-//     element: <Welcome />,
-//   },
-//   {
-//     path: routes.login,
-//     element: <Login />,
-//   },
-//   {
-//     path: routes.signup,
-//     element: <SignUp />,
-//   },
-//   {
-//     element: <PrivateRoutes />,
-//     children: [
-//       {
-//         path: routes.home,
-//         element: <Home />,
-//       },
-//     ],
-//   },
-//   {
-//     path: routes.notFound,
-//     element: <NotFound />,
-//   },
-// ]);
-
 const router = createBrowserRouter([
+  //   {
+  //     path: routes.welcome,
+  //     element: <Welcome />,
+  //   },
   {
-    path: routes.welcome,
+    path: routes.login,
+    element: <Login />,
+  },
+  {
+    path: routes.signup,
+    element: <SignUp />,
+  },
+  {
+    element: <PrivateRoutes />,
     children: [
       {
-        index: true,
-        element: <Welcome />,
-      },
-      {
-        path: routes.login,
-        element: <Login />,
-      },
-      {
-        path: routes.signup,
-        element: <SignUp />,
-      },
-      {
-        element: <PrivateRoutes />,
-        children: [
-          {
-            path: routes.home,
-            element: <Home />,
-          },
-        ],
-      },
-      {
-        path: routes.notFound,
-        element: <NotFound />,
+        path: routes.welcome,
+        element: <Home />,
       },
     ],
+  },
+  {
+    path: routes.notFound,
+    element: <NotFound />,
   },
 ]);
 
