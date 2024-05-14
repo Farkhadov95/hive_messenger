79cd592e3edea4ca2def024238979095e0b36343
Farkhadov95 <farkhadov950405@gmail.com>
2024-05-14 12:12:20 +0500

    switched  to check for token for private route

--- src/router/PrivateRoutes.tsx
+++ src/router/PrivateRoutes.tsx
@@ -1,12 +1,10 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { useUserStore } from "../store/userStore";
 import { routes } from "./Routes";
 
 const PrivateRoutes = () => {
-  const currentUser = useUserStore((state) => state.currentUser);
-  console.log("currentUser", currentUser);
-  console.log("route", routes.login);
-  return currentUser ? <Outlet /> : <Navigate to={`${routes.login}`} />;
+  const token = sessionStorage.getItem("X-Auth-Token");
+  console.log(token);
+  return token ? <Outlet /> : <Navigate to={`${routes.login}`} />;
 };
 
 export default PrivateRoutes;
