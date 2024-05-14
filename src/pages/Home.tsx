8d921196a8ad43418cd8ed78290ce3e40a608a80
Farkhadov95 <farkhadov950405@gmail.com>
2024-05-14 14:05:29 +0500

    Restored Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import { HStack, Show, Box } from "@chakra-ui/react";
+import Chat from "./Chat";
+import { useChatStore } from "../store/chatStore";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useUserStore } from "../store/userStore";
 import { useSocketStore } from "../store/socketStore";
+import Chats from "./Chats";
+import ChatPlaceholder from "./ChatPlaceholder";
 import { URL } from "../services/api";
 
 const Home = () => {
+  const currentChat = useChatStore((state) => state.currentChat);
   const currentUser = useUserStore((state) => state.currentUser);
   const setSocket = useSocketStore((state) => state.setSocket);
   const [socketInitialized, setSocketInitialized] = useState(false);
 
   console.log("Home page is open");
-  console.log(socketInitialized);
 
   useEffect(() => {
     const newSocket = io(URL);
@@ -23,18 +27,12 @@ const Home = () => {
     };
   }, [currentUser, setSocket]);
 
-  // return socketInitialized ? (
-  //   <HStack alignItems={"normal"} maxWidth={"1024px"} margin={"auto"} gap={0}>
-  //     <Chats />
-  //     <Show above="md">{currentChat ? <Chat /> : <ChatPlaceholder />}</Show>
-  //   </HStack>
-  // ) : (
-  //   <Box height={"100vh"} bgColor={"red"}>
-  //     Loading...
-  //   </Box>
-  // );
-
-  return (
+  return socketInitialized ? (
+    <HStack alignItems={"normal"} maxWidth={"1024px"} margin={"auto"} gap={0}>
+      <Chats />
+      <Show above="md">{currentChat ? <Chat /> : <ChatPlaceholder />}</Show>
+    </HStack>
+  ) : (
     <Box height={"100vh"} bgColor={"red"}>
       Loading...
     </Box>
